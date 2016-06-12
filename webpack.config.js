c1d87d53cec76cfab05dc9e00e2b439f324f24af
hyde2able <hyde31415@gmail.com>
2016-06-13 02:50:32 +0900

    add module & set webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,39 @@ const HtmlwebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const PATHS = {
-  bundle: path.join(__dirname, 'app/src/main.js'),
-  style: path.join(__dirname, 'app/styles/main.scss'),
-  build: path.join(__dirname, 'build')
+  bundle: path.resolve(__dirname, 'app/src/main.js'),
+  style: path.resolve(__dirname, 'app/styles/main.scss'),
+  build: path.resolve(__dirname, 'app/build'),
+  dev: path.resolve(__dirname, 'app/build/dev')
 }
 
 module.exports = {
   entry: {
-    bundle: './app/src/main.js'
+    //bundle: './app/src/main.js'
+    bundle: PATHS.bundle
   },
   output: {
-    path: './app/build',
+    path: PATHS.build,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    //new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
       title: 'Listlogs'
     })
   ],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: PATHS.dev,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
   module: {
     loaders: [
       {
